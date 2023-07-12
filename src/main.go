0221c7e01b2da759dbce878c3eae4c6c58adda6a
jigarnayak1290 <jigarnayak1290@gmail.com>
2023-07-13 06:35:15 +0900

    first commit

---
+++ src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	_ "github.com/lib/pq"
+
+	"github.com/jigarnayak1290/goLang/src/handlers"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "Product-api", log.LstdFlags)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	//userRepo := repository.NewUserRepository()
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	C := make(chan os.Signal)
+	signal.Notify(C, os.Interrupt)
+	signal.Notify(C, os.Kill)
+
+	sig := <-C
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+	//http.ListenAndServe(":9090", sm)
+}
